8e372530cbba12566470afdf2f96cb6ceefe0e3d
DmitryKostrov <dimon3limon3@gmail.com>
2022-10-12 14:40:17 +0300

    ver 0.0.0.2

---
+++ Assets/Course Library/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedMove = 10;
+    public float horizontalInput;
+    public float xRange = 10;
+
+    public GameObject projectilePrefab;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speedMove);
+    }
+}
